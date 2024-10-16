afbcf9100769db167abaaa8612101b55d261230d
bokutotu <mushin.hudoushin@gmail.com>
2024-10-16 14:50:29 +0900

    update keymaps

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/default/keymap.c
@@ -29,12 +29,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // A    S    D    F    G              H    J    K    L    BSPC
   // Z    X    C    V    B              N    M    ,    .    RALT
   // LCTL ---  ---  --- SPC  TAB  RGUI ENT ESC BTN1 BTN2 RSFT
-  //                    (L1) (L2) (L2) (L3)
+  //                    (L1) (L2)      (L3)
   [0] = LAYOUT_universal(
     KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
     KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_BSPC  ,
     KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_RALT  ,
-    KC_LCTL  , _______  , _______  , _______  ,LT(1,KC_SPC),LT(2,KC_TAB),     LT(2, KC_RGUI),LT(3,KC_ENT),KC_ESC,KC_BTN1,KC_BTN2, KC_RSFT
+    KC_LCTL  , _______  , _______  , _______  ,LT(1,KC_SPC),LT(2,KC_TAB),     KC_RGUI,LT(3,KC_ENT),KC_ESC,KC_BTN1,KC_BTN2, KC_RSFT
   ),
   // Layer 1: Symbol Layer
   // Provides easy access to various symbols for programming and text editing
@@ -54,15 +54,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // Combines number keys on the top row with additional symbols
   //
   // 1    2    3    4    5              6    7    8    9    0
-  // !    @    #    $    %              ^    &    *    (    )
+  // !    @    #    $    %              ^    &    *    <    >
   // `    ~    _    -    +              =    [    ]    \    |
   // ESC  TAB  LALT ---  SPC  BSPC  --- ENT  .    ,    ;    '
   [2] = LAYOUT_universal(
     KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                            KC_6     , KC_7     , KC_8     , KC_9     , KC_0     ,
-    KC_EXLM  , KC_AT    , KC_HASH  , KC_DLR   , KC_PERC  ,                            KC_CIRC  , KC_AMPR  , KC_ASTR  , KC_LPRN  , KC_RPRN  ,
+    KC_EXLM  , KC_AT    , KC_HASH  , KC_DLR   , KC_PERC  ,                            KC_CIRC  , KC_AMPR  , KC_ASTR  , KC_LT    , KC_GT    ,
     KC_GRV   , KC_TILD  , KC_UNDS  , KC_MINS  , KC_PLUS  ,                            KC_EQL   , KC_LBRC  , KC_RBRC  , KC_BSLS  , KC_PIPE  ,
-    KC_ESC   , KC_TAB   , KC_LALT  , _______  , KC_SPC   , KC_BSPC  ,      _______  , KC_ENT   , KC_DOT   , KC_COMM  , KC_SCLN  , KC_QUOT
-  ),
+    KC_LCTL   , KC_TAB   , KC_LALT  , _______  , KC_SPC   , KC_TAB  ,      KC_RGUI  , KC_ENT   , KC_DOT   , KC_COMM  , KC_SCLN  , KC_QUOT
+  ), 
 
   // Layer 3: Function and Settings Layer (Updated)
   // Includes function keys, RGB controls, keyboard settings, and Vim-like arrow keys
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [3] = LAYOUT_universal(
     KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                            KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   ,
     KC_F11   , KC_F12   , RGB_SAI  , RGB_VAI  , SCRL_DVI ,                            KC_LEFT  , KC_DOWN  , KC_UP    , KC_RIGHT , _______  ,
-    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , SCRL_DVD ,                            CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE ,
+    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , SCRL_DVD ,                            _______  , _______ ,  _______ , _______  , _______ ,
     QK_BOOT  , KBC_RST  , _______  , _______  , _______  , _______  ,      _______  , _______  , _______  , _______  , KBC_RST  , QK_BOOT
   ),
 };
